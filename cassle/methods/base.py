5a4b36d83672f5f573c19d774ab88b737938ad62
lwz <905328134@qq.com>
2023-07-17 20:03:39 +0800

    initial

--- cassle/methods/base.py
+++ cassle/methods/base.py
@@ -413,8 +413,7 @@ class BaseModel(pl.LightningModule):
         class_means = {}
         class_features = {}
         self.eval()
-        for batch in self.train_loaders:
-            _, X_task, Y_task = batch[f"task{self.current_task_idx}"]
+        for _, X_task, Y_task in self.train_loaders[f"task{self.current_task_idx}"]:
             targets = Y_task
             inputs = X_task[0]
             for class_id in self.new_classes:
