571192c1a5653356d2649f9c9ceea7255ce394ff
lwz <905328134@qq.com>
2023-07-17 19:41:02 +0800

    initial

--- job_launcher.py
+++ job_launcher.py
@@ -44,6 +44,7 @@ print(f"Experiment directory: {full_experiment_dir}")
 
 # add experiment directory to the command
 command.extend(["--checkpoint_dir", full_experiment_dir])
+print(command)
 command = " ".join(command)
 
 # run command
